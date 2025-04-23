abbebf76185a90cbc229e47bf41fbcd3f43d65ad
nevadasurfer <jason@kennedyit.com>
2025-04-23 07:57:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,16 @@ git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+reload config with $ source ~/.zshrc
+
+note
+After you source your zsrhc file and run through powerlevel10k config, you get a new ~/.p10k.zsh and updated ~/.zshrc.
+
+this gets added to the bottom of your ~/.zshrc file:
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
